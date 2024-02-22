26ea127a051093cb6970b6900e98a6c8e05c499c
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-02-22 16:59:23 -0300

    This was quite hard, I used ascii
    The solution used regex to filter the non numbers
    and non letters characters. I used ascii logic
    and used a while loop to compare the strings

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+/**
+ * 
+ * @param {string} string 
+ * @returns bool
+ */
+const palindromes = function (string) {
+    const newString = string.toLowerCase().split("").filter(char => {
+        const charVal = char.charCodeAt(0)
+        return (charVal >= 97 && charVal <= 122) || (charVal >= 48 && charVal <= 57)
+    })
+    left = 0
+    right = newString.length - 1
+    while (left < right) {
+        if (newString[left] !== newString[right]) return false
+        left++
+        right--
+    }
+    return true
 };
 
 // Do not edit below this line
