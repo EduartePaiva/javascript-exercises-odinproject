9af581274f48cefdfbf1a61a7fecda7159dba1dd
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-02-23 18:06:54 -0300

    last three questions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (typeof num !== 'number') {
+        num = parseInt(num)
+    }
+    if (num < 0) {
+        return "OOPS"
+    } else if (num == 0) {
+        return 0
+    }
+    let first = 1
+    let second = 1
 
+    for (i = 2; i < num; i++) {
+        const temp = first + second
+        second = first
+        first = temp
+    }
+    return first
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const arrayOfAge = people.map(person => {
+        let death = person.yearOfDeath
+        const birth = person.yearOfBirth
+        if (!death) {
+            death = new Date().getFullYear()
+        }
+        return death - birth
+    })
 
+    const oldestAge = Math.max(...arrayOfAge)
+    return people[arrayOfAge.indexOf(oldestAge)]
 };
 
 // Do not edit below this line
